cd45d4e0535baed7bc3ffc3e7b7815460697a69e
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2018-11-15 18:28:26 +0000

    made camera follow player

---
+++ Buster Brains Colosseum/Assets/Scripts/Follow2D.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow2D : MonoBehaviour {
+
+    public Transform target;
+    public float smoothing = 5.0f;
+
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, -10);
+        transform.position = Vector3.Lerp(transform.position, newPos, (smoothing * .001f));
+	}
+}
