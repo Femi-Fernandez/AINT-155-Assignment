118a3ffa9bc1ee5d5bee1df7de93207936bdfb33
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2018-11-18 15:07:30 +0000

    created zombie prefab, added bullets and gun

---
+++ Buster Brains Colosseum/Assets/Scripts/MoveTowardObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTowardObject : MonoBehaviour {
+    public Transform target;
+    public float speed = 5.0f;
+	// Use this for initialization
+	void Start () {
+        GameObject go = GameObject.FindGameObjectWithTag("PlayerChar");
+        target = go.transform;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (target != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed * 0.01f).normalized;
+        }
+	}
+    public void SetTarget(Transform newTarget)
+    {
+        target = newTarget;
+    }
+}
---
+++ Buster Brains Colosseum/Assets/Scripts/Weapon.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Events;
+
+
+//[System.Serializable]
+//public class onReloadEvent : UnityEvent<bool> { }
+public class Weapon : MonoBehaviour {
+    
+//    public onReloadEvent onReload;
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float fireTime = 0.5f;
+
+
+
+    public int bulletMagazine = 30;
+    public float reloadTime = 1.5f;
+    public bool emptyMagazine = false;
+
+    private bool isFiring = false;
+	// Use this for initialization
+	private void SetFiring()
+    {
+        isFiring = false;
+    }
+	
+    private void Fire()
+    {
+        isFiring = true;
+        Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        Invoke("SetFiring", fireTime);
+    }
+
+    private IEnumerator ReloadGun()
+    {
+            yield return new WaitForSeconds(reloadTime);   
+            bulletMagazine = 30;       
+            emptyMagazine = false;  
+        
+    }
+//    Update is called once per frame
+	void Update () {
+        
+        if (Input.GetMouseButton(0) && !emptyMagazine)
+        {
+            if (!isFiring)
+            {
+              Fire();
+               bulletMagazine = bulletMagazine - 1;
+                if(bulletMagazine == 0)
+                {
+                    emptyMagazine = true;
+                    
+                }
+            }            
+        }
+
+
+        if (Input.GetKeyDown("r"))
+        {
+            StartCoroutine(ReloadGun());
+          //  onReload.Invoke(true);
+        }
+
+    }
+}
