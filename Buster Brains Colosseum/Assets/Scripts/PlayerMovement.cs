718093abfe8fb0c64616722fdd9f71838c2d88bf
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2019-01-11 18:02:51 +0000

    Created the player animations.
    Split the spritesheets into separate segments, then created separate animations for each. Added code to the playermovement script so certain animations play based on the x and y values of the player.

--- Buster Brains Colosseum/Assets/Scripts/PlayerMovement.cs
+++ Buster Brains Colosseum/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,13 @@ public class PlayerMovement : MonoBehaviour {
 
     public float speed = 5.0f;
     Rigidbody2D rigidbody2D;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
 	// Use this for initialization
 	void Start () {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
@@ -17,7 +20,37 @@ public class PlayerMovement : MonoBehaviour {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
+
+        animator.SetFloat("Speed", Mathf.Abs(x));
+
         rigidbody2D.velocity = new Vector2(x, y) * speed;
         rigidbody2D.angularVelocity = 0.0f;
-	}
+
+
+        if (rigidbody2D.velocity.x >= 0 )
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+
+        if (rigidbody2D.velocity.y > 0)
+        {
+            animator.SetBool("Move_up", true);
+        }
+        else
+        {
+            animator.SetBool("Move_up", false);
+        }
+        if (rigidbody2D.velocity.y < 0)
+        {
+            animator.SetBool("Move_down", true);
+        }
+        else
+        {
+            animator.SetBool("Move_down", false);
+        }
+    }
 }
