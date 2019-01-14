a49e43a51d3c19ecd1918c93ebf9e432cb078864
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2019-01-14 21:37:43 +0000

    created bat sprite, bat swing animation, bullet sprite and backgrund tilesheet, as well as the pillar sprite.

--- Buster Brains Colosseum/Assets/Scripts/FollowMouse.cs
+++ Buster Brains Colosseum/Assets/Scripts/FollowMouse.cs
@@ -7,9 +7,16 @@ public class FollowMouse : MonoBehaviour {
     public Camera theCamera;
     public float smoothing = 5.0f;
     public float adjustmentAngle = 0.0f;
+    public SpriteRenderer spriteRenderer;
 
-	// Update is called once per frame
-	void Update () {
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
         Vector3 target = theCamera.ScreenToWorldPoint(Input.mousePosition);
         Vector3 difference = target - transform.position;
 
@@ -17,5 +24,14 @@ public class FollowMouse : MonoBehaviour {
         float rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotz + adjustmentAngle));
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
+
+        //if (rotz < 90)
+        //{
+        //    spriteRenderer.flipX = false;
+        //}
+        //else
+        //{
+        //    spriteRenderer.flipX = true;
+        //}
 	}
 }
---
+++ Buster Brains Colosseum/Assets/Scripts/zombie_animation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_animation : MonoBehaviour {
+
+    Rigidbody2D rigidbody2D;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+    public Vector3 localVelocity;
+    // Use this for initialization
+    void Start () {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        localVelocity = transform.InverseTransformDirection(rigidbody2D.velocity);
+
+        if (localVelocity.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        if (localVelocity.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+}
