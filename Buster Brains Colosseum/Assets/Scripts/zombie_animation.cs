bbc118ee1f955245384884d90286b6762a3a110a
Femi-Fernandez <43548033+Femi-Fernandez@users.noreply.github.com>
2019-01-15 14:55:57 +0000

    made zombies face player

--- Buster Brains Colosseum/Assets/Scripts/zombie_animation.cs
+++ Buster Brains Colosseum/Assets/Scripts/zombie_animation.cs
@@ -8,25 +8,43 @@ public class zombie_animation : MonoBehaviour {
     public Animator animator;
     public SpriteRenderer spriteRenderer;
     public Vector3 localVelocity;
+
+    float oldXPos;
+
     // Use this for initialization
     void Start () {
         rigidbody2D = GetComponent<Rigidbody2D>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        
+        oldXPos = transform.position.x;
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        localVelocity = transform.InverseTransformDirection(rigidbody2D.velocity);
+       
+        float xPos = transform.position.x;
 
-        if (localVelocity.x > 0)
+        if(xPos > oldXPos)
         {
             spriteRenderer.flipX = false;
+            // face right
         }
-        if (localVelocity.x < 0)
+        else
         {
             spriteRenderer.flipX = true;
+            // face left
         }
+        oldXPos = xPos;
+
+        localVelocity = transform.InverseTransformDirection(rigidbody2D.velocity);
+
+        //if (localVelocity.x > 0)
+        //{
+        //    spriteRenderer.flipX = false;
+        //}
+        //if (localVelocity.x < 0)
+        //{
+        //    spriteRenderer.flipX = true;
+        //}
     }
 }
