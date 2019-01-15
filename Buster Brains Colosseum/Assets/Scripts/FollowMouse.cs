7bae14c005d8c82888d664964ca330e812e4f422
Femi Fernandez PC <Femi-Fernandez@users.noreply.github.com>
2019-01-15 12:15:37 +0000

    changed follow mouse so that gun doesnt fire upside fown

--- Buster Brains Colosseum/Assets/Scripts/FollowMouse.cs
+++ Buster Brains Colosseum/Assets/Scripts/FollowMouse.cs
@@ -7,11 +7,10 @@ public class FollowMouse : MonoBehaviour {
     public Camera theCamera;
     public float smoothing = 5.0f;
     public float adjustmentAngle = 0.0f;
-    public SpriteRenderer spriteRenderer;
+    public float rotz;
 
     private void Start()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
+    {  
 
     }
 
@@ -21,17 +20,17 @@ public class FollowMouse : MonoBehaviour {
         Vector3 difference = target - transform.position;
 
         difference.Normalize();
-        float rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        rotz = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         Quaternion newRotation = Quaternion.Euler(new Vector3(0.0f, 0.0f, rotz + adjustmentAngle));
         transform.rotation = Quaternion.Lerp(transform.rotation, newRotation, Time.deltaTime * smoothing);
 
-        //if (rotz < 90)
-        //{
-        //    spriteRenderer.flipX = false;
-        //}
-        //else
-        //{
-        //    spriteRenderer.flipX = true;
-        //}
+        if ((rotz < 90)&&(rotz > -90))
+        {
+            transform.Rotate(new Vector3(0, 0, 0));
+        }
+        else
+        {
+            transform.Rotate(new Vector3(0, 180, 0));
+        }
 	}
 }
